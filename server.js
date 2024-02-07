5ef6721817c53a303a473f862abad8670c66a0b8
yogeshprakhar <yogeshprakharpal200027@gmail.com>
2024-02-07 23:41:44 +0530

    changes for production

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const ACTIONS = require("./src/Actions");
+const path = require("path");
 
 const server = http.createServer(app);
 const io = new Server(server);
+
+app.use(express.static("build"));
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname,'build','index.html'));
+});
 
 const userSocketMap = {};
 function getAllConnectedClients(roomId) {
@@ -24,7 +30,7 @@ io.on("connection", (socket) => {
 
   // when client joined from frontend , frontend socket send roomid and username to node server(node server also have socket which recieve from frontend)
 
-  // now node server have all client list so i will inform all client , so i will send data form node server(socket server) to frontend(socket) 
+  // now node server have all client list so i will inform all client , so i will send data form node server(socket server) to frontend(socket)
   socket.on(ACTIONS.JOIN, ({ roomId, userName }) => {
     // console.log("userName is ",userName)
     userSocketMap[socket.id] = userName;
@@ -40,26 +46,26 @@ io.on("connection", (socket) => {
     });
   });
 
-  socket.on(ACTIONS.CODE_CHANGE, ({roomId, code})=>{
-    socket.in(roomId).emit(ACTIONS.CODE_CHANGE, {code});
-  })
+  socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
+    socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
+  });
 
   socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
     io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
   });
-  
-// if someone discoonect server will notify and emit Action to frontend client socket
-  socket.on('disconnecting', ()=>{
-    const rooms = [...socket.rooms]
-    rooms.forEach((roomId)=>{
-        socket.in(roomId).emit(ACTIONS.DISCONNECTED,{
-            socketId : socket.id,
-            userName: userSocketMap[socket.id],
-        })
-    })
-    delete userSocketMap[socket.id]
+
+  // if someone discoonect server will notify and emit Action to frontend client socket
+  socket.on("disconnecting", () => {
+    const rooms = [...socket.rooms];
+    rooms.forEach((roomId) => {
+      socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+        socketId: socket.id,
+        userName: userSocketMap[socket.id],
+      });
+    });
+    delete userSocketMap[socket.id];
     socket.leave();
-  })
+  });
 });
 
 const PORT = process.env.PORT || 5000;
