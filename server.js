f3bd88534238e24f4f9c3fe4978030422f758d65
yogeshprakhar <yogeshprakharpal200027@gmail.com>
2024-02-10 22:09:08 +0530

    commits

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const path = require("path");
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("build"));
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname,'build','index.html'));
-});
+// app.use(express.static("build"));
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname,'build','index.html'));
+// });
 
 const userSocketMap = {};
 function getAllConnectedClients(roomId) {
